e9192176f7f3cc53f59943ac6496004d78188d06
Damian <damian.skarbiecki@gmail.com>
2023-11-19 17:28:55 +0100

    Room database added

---
+++ Projekt_BAM/app/src/main/java/com/example/projekt_bam/UserDao.kt
@@ -0,0 +1,18 @@
+package com.example.projekt_bam
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface UserDao {
+    @Insert
+    fun insertUser(user: UserEntity)
+
+    @Query("SELECT * FROM users WHERE email = :email")
+    fun getUserByEmail(email: String): UserEntity?
+
+    @Query("SELECT * FROM users")
+    fun getAllUsers(): List<UserEntity>
+}
---
+++ Projekt_BAM/app/src/main/java/com/example/projekt_bam/UserEntity.kt
@@ -0,0 +1,27 @@
+package com.example.projekt_bam
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "users")
+data class UserEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+    @ColumnInfo(name = "email")
+    val email: String,
+    @ColumnInfo(name = "password")
+    val password: String,
+    @ColumnInfo(name = "first_name")
+    val firstName: String,
+    @ColumnInfo(name = "last_name")
+    val lastName: String,
+    @ColumnInfo(name = "pesel")
+    val pesel: String,
+    @ColumnInfo(name = "address")
+    val address: String,
+    @ColumnInfo(name = "id_number")
+    val idNumber: String,
+    @ColumnInfo(name = "phone_number")
+    val phoneNumber: String
+)
